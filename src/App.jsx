61fd82b5b8db660e5160bff422880ee527b18269
Carlos Eduardo Ortiz <carlosklinton.2016@gmail.com>
2023-12-05 11:24:04 -0500

    se puso identaciones

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import { useMessages } from './hooks/useMessages'
 function App() {
 
 const {user, messages, useUid, handleEditMode, handleDeleteMessage,
-   editMode, setEditMode, info, setInfo, login, logOut } = useMessages()
+        editMode, setEditMode, info, setInfo, login, logOut } = useMessages()
 
 const handleChangeFields = (e) => setInfo(e.target.value)
-if(messages.length < 1){
-  return <div className="d-flex  align-items-center min-vh-100 ">
-         <Charging/>
-        </div>
-}
+
+  if(messages.length < 1){
+    return <div className="d-flex  align-items-center min-vh-100 ">
+                <Charging/>
+          </div>
+  }
+
   return ( 
         <div className=' min-vh-100 container'>
               {
